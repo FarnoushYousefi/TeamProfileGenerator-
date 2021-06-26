30e17aa20b9e6d20edfb71f71fa2871d28e8fc6c
Farnoush <farnoush.yousefikejani@uwaterloo.ca>
2021-06-26 14:09:40 -0400

    add the css

--- index.js
+++ index.js
@@ -137,14 +137,42 @@ function addEngineer() {
     .catch((error) => {});
 }
 function evaluteAdditionalTeamMemberResult(result) {
-  if (answer.additionalTeamMember == 'Engineer') {
+  console.log('farnoush');
+  if (result == 'Engineer') {
     addEngineer();
-  } else if (answer.additionalTeamMember == 'Intern') {
+  } else if (result == 'Intern') {
     addIntern();
   } else {
     generateHTML();
   }
 }
 function generateHTML() {
-  fs.writeFileSync(gernerateHtmlFilePath, '');
+  console.log(teamMember);
+  let HTML = ` <head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+    rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+    crossorigin="anonymous"
+  />
+  <title>teamprofile</title>
+</head>
+`;
+  teamMember.forEach(
+    (member) =>
+      (HTML += ` <div class="card w-75">
+  <div class="card-body">
+    <h5 class="card-title">${member.name}</h5>
+    <p class="card-text">
+      With supporting text below as a natural lead-in to additional content.
+    </p>
+    <a href="#" class="btn btn-primary">Button</a>
+  </div>
+</div>`)
+  );
+  console.log(HTML);
+  fs.writeFileSync(gernerateHtmlFilePath, HTML);
 }
