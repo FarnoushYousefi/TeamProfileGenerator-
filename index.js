a53111912e2ffc4a287f40be1a22b3be1649b252
Farnoush <farnoush.yousefikejani@uwaterloo.ca>
2021-06-26 09:54:15 -0400

    add the intern function

--- index.js
+++ index.js
@@ -1,9 +1,146 @@
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
-// const inquirer = require('inquirer');
-let manager = new Manager('f', 'g', 'e', '09');
-console.log(manager.getName());
-console.log(manager.getId());
-console.log(manager.getEmail());
-console.log(manager.getOfficeNumber());
+//
+// require file system to generate the HTMl
+const fs = require('fs');
+//generate the HTML file path
+
+// global Object to hold the team members
+let teamMember = [];
+
+const gernerateHtmlFilePath = './dist/teamprofile.html';
+inquirer
+  .prompt([
+    {
+      name: 'managerName',
+      type: 'input',
+      message: "Enter team manager's name",
+    },
+    {
+      name: 'managerId',
+      type: 'input',
+      message: "Enter team manager's ID",
+    },
+    {
+      name: 'managerEmail',
+      type: 'input',
+      message: "Enter team manager's Email",
+    },
+    {
+      name: 'managerOfficeNumber',
+      type: 'input',
+      message: "Enter team manager's Office Number",
+    },
+    {
+      name: 'additionalTeamMember',
+      type: 'list',
+      message: 'select team members to add',
+      choices: ['Engineer', 'Intern'],
+    },
+  ])
+  .then((answer) => {
+    fs.writeFileSync(gernerateHtmlFilePath, '');
+  })
+  .catch((error) => {});
+// call this as many times as members Intern need to bed added
+function addIntern() {
+  inquirer
+    .prompt([
+      {
+        name: 'InternName',
+        type: 'input',
+        message: "Enter team Intern's name",
+      },
+      {
+        name: 'InternId',
+        type: 'input',
+        message: "Enter team Intern's ID",
+      },
+      {
+        name: 'InternEmail',
+        type: 'input',
+        message: "Enter Intern's Email",
+      },
+      {
+        name: 'internSchool',
+        type: 'input',
+        message: "Enter Intern's school",
+      },
+      {
+        name: 'additionalTeamMember',
+        type: 'list',
+        message: 'select team members to add',
+        choices: ['Engineer', 'Intern', 'Exit'],
+      },
+    ])
+    .then((answer) => {
+      let intern = new Intern(
+        answer.InternName,
+        answer.InternId,
+        answer.InternEmail,
+        answer.internSchool
+      );
+      teamMember.push(intern);
+      if (answer.additionalTeamMember == 'Engineer') {
+        addEngineer();
+      } else if (answer.additionalTeamMember == 'Intern') {
+        addIntern();
+      } else {
+        generateHTML();
+      }
+    })
+    .catch((error) => {});
+}
+
+// call it as many engineer need to add
+function addEngineer() {
+  inquirer
+    .prompt([
+      {
+        name: 'InternName',
+        type: 'input',
+        message: "Enter team Intern's name",
+      },
+      {
+        name: 'InternId',
+        type: 'input',
+        message: "Enter team Intern's ID",
+      },
+      {
+        name: 'InternEmail',
+        type: 'input',
+        message: "Enter Intern's Email",
+      },
+      {
+        name: 'internSchool',
+        type: 'input',
+        message: "Enter Intern's school",
+      },
+      {
+        name: 'additionalTeamMember',
+        type: 'list',
+        message: 'select team members to add',
+        choices: ['Engineer', 'Intern', 'Exit'],
+      },
+    ])
+    .then((answer) => {
+      let intern = new Intern(
+        answer.InternName,
+        answer.InternId,
+        answer.InternEmail,
+        answer.internSchool
+      );
+      teamMember.push(intern);
+      if (answer.additionalTeamMember == 'Engineer') {
+        addEngineer();
+      } else if (answer.additionalTeamMember == 'Intern') {
+        addIntern();
+      } else {
+        generateHTML();
+      }
+    })
+    .catch((error) => {});
+}
+
+function generateHTML() {}
